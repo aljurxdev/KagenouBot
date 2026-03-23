d638fa830dce9bfb1ec31e2c628f4656b937f3ff
aljurx_z <aljurdev@gmail.com>
2026-03-23 23:42:35 +0800

    Update run.js

--- run.js
+++ run.js
@@ -2,12 +2,18 @@ require("./utils/logger");
 const { spawn } = require("child_process");
 const fs   = require("fs");
 const path = require("path");
+
 const configPath = path.join(__dirname, "config.json");
 const config     = JSON.parse(fs.readFileSync(configPath, "utf8"));
 
-const script = config.DiscordMode ? "./Discord/index" : "./index";
+const script = config.DiscordMode
+  ? "./Discord/index"
+  : "./index";
 
-global.log.info(`[RUN] Starting ${config.DiscordMode ? "Discord-KagenouBot" : "fb-KagenouBot"} bot…`);
+global.log.info(`[RUN] Starting ${
+  config.DiscordMode ? "Discord-KagenouBot" :
+  "FB-KagenouBot"
+} bot…`);
 
 const child = spawn("node", [script], {
   stdio: "inherit",
